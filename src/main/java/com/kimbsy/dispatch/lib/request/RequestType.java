f1e5c0e5014d181601559de2330dbbb3892b0614
Kimbsy <lordkimber@gmail.com>
2017-11-05 11:19:05 +0000

    initial commit, factored request package out of dispatch and alexecute projects

---
+++ src/main/java/com/kimbsy/dispatch/lib/request/RequestType.java
@@ -0,0 +1,12 @@
+package com.kimbsy.dispatch.lib.request;
+
+/**
+ * Lists the different types of requests available.
+ *
+ * @author kimbsy
+ */
+public enum RequestType {
+
+    SYSTEM_OUT_PRINT,
+    WATCH_NEXT,
+}
